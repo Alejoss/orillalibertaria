d20fed88c0aee0f53ee215878ebcd91fd4e6f2e6
AlejoLinux <alejoveintimilla@gmail.com>
2014-07-28 10:27:39 -0500

    remover el field nickname por ahora

--- perfiles/models.py
+++ perfiles/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 class Perfiles(models.Model):
     usuario = models.OneToOneField(User, unique=True, null=True)
-    nickname = models.CharField(max_length=75, null=True)
     descripcion = models.CharField(max_length=250, blank=True, null=True)
     votos_recibidos = models.SmallIntegerField(default=0)
     numero_de_posts = models.SmallIntegerField(default=0)
