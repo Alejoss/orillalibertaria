d02dadc444aa3f50ceba33408ebf04e2f75c3e10
AlejoLinux <alejoveintimilla@gmail.com>
2014-07-30 18:10:15 -0500

    cambio field imagen a CharField

--- perfiles/models.py
+++ perfiles/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Perfiles(models.Model):
     usuario = models.OneToOneField(User, unique=True, null=True)
     nickname = models.CharField(max_length=75, null=True)
-    imagen_perfil = models.ImageField(upload_to='fotos_perfil/', null=True)
+    imagen_perfil = models.CharField(max_length=75, null=True)
     descripcion = models.CharField(max_length=250, blank=True, null=True)
     votos_recibidos = models.SmallIntegerField(default=0)
     numero_de_posts = models.SmallIntegerField(default=0)
