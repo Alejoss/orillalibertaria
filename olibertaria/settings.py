ebd1fbaf2ac127da9779f05619c75f2d823a38bb
AlejoLinux <alejoveintimilla@gmail.com>
2014-09-17 16:27:59 -0500

    arreglar imagenes. sumar favicon

--- olibertaria/settings.py
+++ olibertaria/settings.py
@@ -28,14 +28,11 @@ TEMPLATE_DIRS = (
     join(BASE_DIR,  'templates'),
 )
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["OL_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TEMPLATE_DEBUG = False
+DEBUG = TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
