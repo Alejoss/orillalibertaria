aae1f3dd8a8fcb3336e536469437edd8bbfa6384
AlejoLinux <alejoveintimilla@gmail.com>
2014-09-18 14:51:52 -0500

    new relic requirements

--- olibertaria/settings.py
+++ olibertaria/settings.py
@@ -32,7 +32,7 @@ TEMPLATE_DIRS = (
 SECRET_KEY = os.environ["OL_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TEMPLATE_DEBUG = True
+DEBUG = TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
