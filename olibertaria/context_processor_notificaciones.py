8e1cd5a77c20856e390ef29d831db04468bcb32b
AlejoLinux <alejoveintimilla@gmail.com>
2015-03-24 21:23:55 -0500

    commit raro

--- olibertaria/context_processor_notificaciones.py
+++ olibertaria/context_processor_notificaciones.py
@@ -5,10 +5,15 @@ from temas.models import Perfiles
 def procesar_notificaciones(request):
     context = {}
     if request.user.is_authenticated():
-        perfil_usuario = Perfiles.objects.get(usuario=request.user)
-        notificaciones = obtener_notificaciones(perfil_usuario)
-        num_notificaciones = obtener_num_notificaciones(perfil_usuario)
-        context = {'notificaciones': notificaciones,
+        perfil_usuario, created = Perfiles.objects.get_or_create(usuario=request.user)
+
+        if created:
+            return context
+
+        else:
+            notificaciones = obtener_notificaciones(perfil_usuario)
+            num_notificaciones = obtener_num_notificaciones(perfil_usuario)
+            context = {'notificaciones': notificaciones,
                    'num_notificaciones': num_notificaciones}
 
         return context
--- olibertaria/urls.py
+++ olibertaria/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.static import static
+
 from temas import views
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        # Python social auth
+                       url(r'^admin/', include(admin.site.urls)),
                        url('', include('social.apps.django_app.urls', namespace='social')),
                        # Redirect a main si accede el usuario a orillalibertaria.com
                        url(r'^$', views.inicio, name="inicio_redirect"),
